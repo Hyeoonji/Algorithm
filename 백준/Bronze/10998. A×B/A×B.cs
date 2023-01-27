fa80079774497b2bae7681e2eac1a5c919f5e2c4
Shin Hyeon Ji <122066084+Hyeoonji@users.noreply.github.com>
2023-01-27 13:03:47 +0900

    [Bronze V] Title: A×B, Time: 64 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cs
@@ -0,0 +1,17 @@
+using System;
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+            
+            int a = int.Parse(ss[0]);
+            int b = int.Parse(ss[1]);
+            
+            Console.WriteLine(a*b);
+        }
+    }
+}
