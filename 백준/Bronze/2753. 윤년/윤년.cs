147af4c4e5a3b938f13521ce53735cb3db4e8e3e
Shin Hyeon Ji <122066084+Hyeoonji@users.noreply.github.com>
2023-01-27 13:14:58 +0900

    [Bronze V] Title: 윤년, Time: 64 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cs
@@ -0,0 +1,16 @@
+using System;
+namespace baekjoon{
+    class Program{
+        static void Main(){
+            int N = int.Parse(Console.ReadLine());
+            if (((N % 4 == 0) & (N % 100 != 0)) || (N % 400 ==0))
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {            
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
