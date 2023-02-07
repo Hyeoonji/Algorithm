860a4f9ae72964385cfb9dbb8b68ec827ee7c03d
Shin Hyeon Ji <jane49621@gmail.com>
2023-02-07 11:21:15 +0900

    4-11

--- 4.cs
+++ 4.cs
@@ -221,51 +221,56 @@
 //        }
 //    }
 //}
-    
+
 //-----------------------
 // foreach 끝까지 알아서 순환하는 반복문
 // foreach(데이터타입 변수명 in 배열,컬렉션등)
 
 // 4344. 평균은 넘겠지
+//public class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        // 첫째줄에는 테스트 케이스의 개수 C
+//        int tcase = int.Parse(Console.ReadLine());
 
-public class Program
-{
-    static void Main(string[] args)
-    {
-        // 첫째줄에는 테스트 케이스의 개수 C
-        int tcase = int.Parse(Console.ReadLine());
+//        // 테스크 케이스마다 학생의 수 N + N명의 점수
+//        for (int i = 0; i < tcase; i++)
+//        {
+//            string[] str = Console.ReadLine().Split(' ');
+//            // 학생의 수 N
+//            int num = int.Parse(str[0]);
+//            // 첫번째 학생의 수를 제외한 나머지 점수
+//            int[] score = new int[num+ 1];
+//            float sum = 0; // 합계
+//            float ave = 0; // 평균
 
-        // 테스크 케이스마다 학생의 수 N + N명의 점수
-        for(int i = 0; i < tcase; i++)
-        {
-            string[] str = Console.ReadLine().Split(' ');
-            // 학생의 수 N
-            int n = int.Parse(str[0]);
-            // 첫번째 학생의 수를 제외한 나머지 점수
-            int[] score = new int[n+1];
-            // 평균
-            int [] ave = new int[i];
-            // 평균을 구하기 위한 점수 합
-            int[] sum = new int[i];
+//            for (int j = 0; j < num+1; j++)
+//            {
+//                // 점수에 연결, 합계 구하기
+//                score[j] = int.Parse(str[j]);
+//                if(j >= 1)
+//                {
+//                    sum += score[j];
+//                }
 
-            for (int j =0; j<score.Length;j++)
-            {
-                sum[i] += score[j];
-                ave[i] = sum[i] / n;
-                // 인원을 구하기 위한 변수
-                int count = 0;
+//            }
+//            ave = sum / (float)num;
 
-                if (score[j] >= ave[i])
-                {
-                    count++;
+//            // 평균이 넘는 학생의 수
+//            float count = 0;
+//            for (int j = 0; j < num+1; j++)
+//            {
+//                // 점수가 평균보타 크다면
+//                if (j >= 1 && score[j] > ave)
+//                {
+//                    count++;
+//                }
+//            }
 
-                }
-
-            }
-            
-
-
-        }
-        // 각 케이스마다 평균이 넘는 학생의 비율을 출력 소수 셋째자리까지 반올림
-    }
-}
+//            float result = count / (float)num;
+//            // 각 케이스마다 평균이 넘는 학생의 비율을 출력 소수 셋째자리까지 반올림
+//            Console.WriteLine("{0:#0.000%}", result);
+//        }
+//    }
+//}
---
+++ 5.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon
+{
+    internal class _5
+    {
+    }
+}
