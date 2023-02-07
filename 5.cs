541736da8232dacafb5c440da31fad8ede76fc23
Shin Hyeon Ji <jane49621@gmail.com>
2023-02-07 12:29:56 +0900

    5-2

--- 5.cs
+++ 5.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
-namespace Baekjoon
+namespace Baekjoon;
+
+//4673. 셀프 넘버
+public class Program
 {
-    internal class _5
+    // 생성자 n
+
+    // 함수 d(n)
+    static int Creater(string num)
     {
+        // 받은 숫자를 결과에 저장
+        int result = int.Parse(num);
+        for(int i=0; i<num.Length; i++) 
+        {
+            // 추가적으로 숫자의 각 자리수를 더함
+            result += int.Parse(num[i].ToString());
+        }
+        return result;
     }
+
+    // 수열에서 n이 없는 셀프넘버 찾아내기
+    // 함수에 대입했을 떄 없는 숫자 찾아내기
+    // 여기서는 10000개까지로 지정
+
+    static void Main(string[] args)
+    {
+        // 1부터 10000까지 받아서
+        int[] value = new int[10000];
+        // 하나씩 다 함수넣어보기
+        for(int i=0; i< 10000; i++)
+        {
+            value[i] = Creater(i.ToString());
+        }
+
+        // 비교를 위해 값을 받을 StringBuilder
+        StringBuilder sb = new StringBuilder();
+
+        // 1-10000과 아까 넣었던 value 비교하기
+        // 같으면 bool을 통해 확인
+        
+
+
+    }
+
 }
+//-------------------------------
+// ToString(): 값이나 정보를 문자열로 리턴하는 메소드
+// StringBuilder(): 변경가능한 문자열, 새로운 StringBuilder 인스턴스 초기화
+
