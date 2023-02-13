92830a80ab6da18cb434ebc57f7f8323bf9e8594
Shin Hyeon Ji <jane49621@gmail.com>
2023-02-13 13:34:44 +0900

    5-3.6-1

---
+++ 6.cs
@@ -0,0 +1,27 @@
+﻿namespace Baekjoon;
+
+// 11654. 아스키코드
+//public class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        //첫째 줄에 숫자 입력받기
+//        char num = Convert.ToChar(Console.ReadLine());
+//        // 아스키코드로 출력하기
+//        int ascii = Convert.ToInt32(num);
+//        Console.WriteLine(ascii);
+//    }
+//}
+
+// 11720. 숫자의 합
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // 첫째줄에 숫자의 개수n
+        int n = int.Parse(Console.ReadLine());
+        // 둘째줄에 숫자가 개수만큼 공백없이 주어짐
+        // 숫자 n개의 합 출력
+    }
+}
+
