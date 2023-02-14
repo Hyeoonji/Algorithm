277eae5b6889fbbf2c26e3158e7fbc083fe03902
Shin Hyeon Ji <jane49621@gmail.com>
2023-02-14 13:05:33 +0900

    6-2

--- 6.cs
+++ 6.cs
@@ -14,14 +14,41 @@
 //}
 
 // 11720. 숫자의 합
+//public class Program
+//{
+//    // 기존의 숫자 합과의 차이점: 공백
+//    static void Main(string[] args)
+//    {
+//        // 첫째줄에 숫자의 개수n
+//        int n = int.Parse(Console.ReadLine());
+//        // 둘째줄에 숫자가 개수만큼 공백없이 주어짐
+//        string str = Console.ReadLine();
+
+//        // 합을 받을 변수
+//        int count = 0;
+//        // 숫자 n개의 합 출력하기
+//        for(int i =0; i < n; i++)
+//        {
+//            count += int.Parse(str[i].ToString());
+//        }
+//        Console.WriteLine(count);
+
+//    }
+//}
+
+// 10809. 알파벳찾기
 public class Program
 {
     static void Main(string[] args)
     {
-        // 첫째줄에 숫자의 개수n
-        int n = int.Parse(Console.ReadLine());
-        // 둘째줄에 숫자가 개수만큼 공백없이 주어짐
-        // 숫자 n개의 합 출력
+        // 첫째 줄에 단어 S
+        string word = Console.ReadLine();
+        // 알파벳의 위치를 표현
+        for(int i =0;i<26;i++)
+        {
+        }
+        // 포함되어 있지 않다면 -1
+
     }
 }
 
