381a0540bd7e0749fef84cf24d0afb2c70cf3b57
Shin Hyeon Ji <jane49621@gmail.com>
2023-02-16 12:02:38 +0900

    6-3

--- 6.cs
+++ 6.cs
@@ -37,18 +37,41 @@
 //}
 
 // 10809. 알파벳찾기
+//public class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        // 첫째 줄에 단어 S
+//        string word = Console.ReadLine();
+//        // 알파벳의 위치를 표현
+//        // 아스키코드를 활용: 소문자 a-z = 97- 122
+//        for(int i =97 ; i<=122 ; i++)
+//        {
+//            // 기본값 -1
+//            int result = -1;
+//            for (int j = 0; j < word.Length; j++)
+//            {
+//                //97부터 같은지 확인
+//                if(i == word[j])
+//                {
+//                    // 같으면 자릿수 반환
+//                    result = j;
+//                    break;
+//                }
+//            }
+//            // 공백으로 구분해서 출력
+//            Console.Write(result + " ");
+//        }
+//    }
+//}
+
+// 2675. 문자열 반복
 public class Program
 {
     static void Main(string[] args)
     {
-        // 첫째 줄에 단어 S
-        string word = Console.ReadLine();
-        // 알파벳의 위치를 표현
-        for(int i =0;i<26;i++)
-        {
-        }
-        // 포함되어 있지 않다면 -1
-
+        // 첫째줄에 테스트 케이스의 개수
+        // 
     }
 }
 
