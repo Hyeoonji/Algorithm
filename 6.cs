d8f6653ca02d59430cf0ad6d611d3f674fe3c65b
Shin Hyeon Ji <jane49621@gmail.com>
2023-02-16 14:14:02 +0900

    6-4

--- 6.cs
+++ 6.cs
@@ -1,4 +1,6 @@
-﻿namespace Baekjoon;
+﻿using System.Text;
+
+namespace Baekjoon;
 
 // 11654. 아스키코드
 //public class Program
@@ -71,7 +73,29 @@ public class Program
     static void Main(string[] args)
     {
         // 첫째줄에 테스트 케이스의 개수
-        // 
+        int test = int.Parse(Console.ReadLine());
+
+        // 케이스 만큼 반복
+        for(int i =0; i < test; i++)
+        {
+            // 반복 횟수 R + 공백 + 반복할 문자열 S
+            // 테스트할 문자열 받기
+            string[] str = Console.ReadLine().Split();
+            // 반복횟수
+            int r = int.Parse(str[0]);
+            
+            // str
+            for(int j =0; j < str[1].Length; j++)
+            {
+                // r만큼 반복
+                for(int k = 0; k < r; k++)
+                {
+                    Console.Write(str[1][j]);
+                }
+            }
+            Console.WriteLine();
+
+        }
     }
 }
 
