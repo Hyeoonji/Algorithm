3121d2d590989df6e5790ede248fe163b3f6a071
Shin Hyeon Ji <jane49621@gmail.com>
2023-02-21 10:39:13 +0900

    1-9.11.12.13

--- 1.cs
+++ 1.cs
@@ -11,5 +11,80 @@ namespace Baekjoon;
 //    }
 //}
 
-// 
+// 10430. 나머지
+//public class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        // 첫째줄에 순서대로 ABC 사이 공백
+//        string[] str = Console.ReadLine().Split();
+//        int A = int.Parse(str[0]);
+//        int B = int.Parse(str[1]);
+//        int C = int.Parse(str[2]);
+//        // 첫째 줄에 (A+B)%C
+//        Console.WriteLine((A + B) % C);
+//        // 둘째 줄에 ((A%C) + (B%C))%C
+//        Console.WriteLine(((A % C) + (B % C)) % C);
+//        // 셋째 줄에 (A×B)%C
+//        Console.WriteLine((A*B) % C);
+//        // 넷째 줄에 ((A%C) × (B%C))%C 출력
+//        Console.WriteLine(((A % C) * (B % C)) % C );
+//    }
+//}
+
+// 2588. 곱셈
+public class Program 
+{
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+    }
+}
+
+
+// 11382. 꼬마 정민
+//public class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        string[] str = Console.ReadLine().Split(" ");
+//        double A = double.Parse(str[0]);
+//        double B = double.Parse(str[1]);
+//        double C = double.Parse(str[2]);
+
+//        Console.WriteLine(A + B + C);
+//    }
+//}
+
+// 10171. 고양이
+//public class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("\\    /\\");
+//        Console.WriteLine(" )  ( ')");
+//        Console.WriteLine("(  /  )");
+//        Console.WriteLine(" \\(__)|");
+//    }
+//}
+
+//------------------------------
+// \: 줄바꿈 기호
+// \를 한번 출력하기위해서는 두번 입력해야한다.
+
+// 10172. 개
+//public class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("|\\_/|");
+//        Console.WriteLine("|q p|   /}");
+//        Console.WriteLine("( 0 )\"\"\"\\");
+//        Console.WriteLine("|\"^\"`    |");
+//        Console.WriteLine("||_/=\\\\__|");
+//    }
+//}
+
 
