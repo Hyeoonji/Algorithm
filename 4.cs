0487231026bdcd213d433dc9d0cd3f149693eed0
Shin Hyeon Ji <jane49621@gmail.com>
2023-02-24 13:08:09 +0900

    4-5

--- 4.cs
+++ 4.cs
@@ -113,6 +113,43 @@ namespace Baekjoon;
 //}
 
 // 10810. 공 넣기
+//public class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        // 첫째줄에 바구니 개수 N 진행횟수 M
+//        string[] str = Console.ReadLine().Split();
+//        int N = int.Parse(str[0]);
+//        int M = int.Parse(str[1]);
+
+//        // 공의 번호를 받을 배열
+//        int[] count = new int[N];
+
+//        for (int i = 0; i < N; i++)
+//        {
+//            count[i] = 0;
+//        }
+
+//        // 둘째줄부터 ( 시작바구니i 끝바구니j 넣을공의번호k ) 를 M만큼 반복
+//        for (int i = 0; i < M; i++)
+//        {
+//            string[] Case = Console.ReadLine().Split();
+//            int start = int.Parse(Case[0]);
+//            int End = int.Parse(Case[1]);
+
+//            for(int j = start-1; j <= End-1; j++)
+//            {
+//                count[j] = int.Parse(Case[2]);
+//            }
+//        }
+//        foreach (int number in count)
+//        {
+//            Console.Write(number + " ");
+//        }
+//    }
+//}
+
+// 10813. 공 바꾸기
 public class Program
 {
     static void Main(string[] args)
@@ -125,27 +162,33 @@ public class Program
         // 공의 번호를 받을 배열
         int[] count = new int[N];
 
+        // 1부터 넣어주기
         for (int i = 0; i < N; i++)
         {
-            count[i] = 0;
+            count[i] = i+1;
         }
 
-        // 둘째줄부터 ( 시작바구니i 끝바구니j 넣을공의번호k ) 를 M만큼 반복
+        // 둘째줄부터(바구니i 바구니j) 를 M만큼 반복
         for (int i = 0; i < M; i++)
         {
             string[] Case = Console.ReadLine().Split();
-            int start = int.Parse(Case[0]);
+            int Start = int.Parse(Case[0]);
             int End = int.Parse(Case[1]);
-            int num = int.Parse(Case[2]);
 
-            for(int j = start; j <= End; j++)
-            {
-                count[i] = num;
-            }
+            //for (int j = Start - 1; j <= End - 1; j++)
+            //{
+            //    count[i] = count[End];
+            //    count[End] = count[Start];
+
+            //}
         }
+        foreach (int number in count)
+        {
+            Console.Write(number + " ");
+        }
+
     }
 }
-
 
 
 // 5597. 과제 안 내신 분,,?
