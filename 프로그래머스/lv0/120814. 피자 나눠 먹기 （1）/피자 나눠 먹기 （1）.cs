cda7c8004f6a22ce7c5730aaeeefe72be3f8b1ec
Shin Hyeon Ji <122066084+Hyeoonji@users.noreply.github.com>
2023-03-26 13:56:33 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.22 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = n/7;
+        if(n%7 > 0 ){
+            answer ++;
+        }
+        return answer;
+    }
+}
