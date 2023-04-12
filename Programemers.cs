d5889c7a5e22fe1672d99eddbba11630d070279e
Shin HyeonJi <jane49621@gmail.com>
2023-04-12 21:17:23 +0900

    제곱근 판별, 특정 문자 제거하기

---
+++ Programemers.cs
@@ -0,0 +1,60 @@
+﻿namespace Programmers;
+
+// 정수 제곱근 판별
+//public class Solution
+//{
+//    public long solution(long n)
+//    {
+//        long answer = 0;
+//        for (long i = 0; i <= n; i++)
+//        {
+//            if(i*i == n)
+//            {
+//                answer = (i + 1) * (i + 1);
+//                break;
+//            } else if(i*i == 0)
+//            {
+//                answer = -1;
+//            }
+//        }
+
+//        return answer;
+//    }
+//}
+
+// 특정 문자 제거하기
+//using System;
+
+//public class Solution
+//{
+//    public string solution(string my_string, string letter)
+//    {
+//        string answer = "";
+//        foreach (char text in my_string)
+//        {
+//            if (text.ToString() == letter)
+//            {
+//                continue;
+//            }
+//            answer += $"{text}";
+//        }
+//        return answer;
+//    }
+//}
+
+// 행렬의 덧셈
+public class Solution
+{
+    public int[,] solution(int[,] arr1, int[,] arr2)
+    {
+        int[,] answer = new int[arr1.GetLength(0),arr2.GetLength(1)];
+        
+
+        return answer;
+    }
+}
+
+
+
+
+
