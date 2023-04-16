c983b5d47e03781e68d49dc2f87cd896ee0db3c2
Shin HyeonJi <jane49621@gmail.com>
2023-04-16 21:04:53 +0900

    0416 3문제

--- Programemers.cs
+++ Programemers.cs
@@ -43,16 +43,39 @@
 //}
 
 // 행렬의 덧셈
-public class Solution
-{
-    public int[,] solution(int[,] arr1, int[,] arr2)
-    {
-        int[,] answer = new int[arr1.GetLength(0),arr2.GetLength(1)];
-        
+//public class Solution
+//{
+//    public int[,] solution(int[,] arr1, int[,] arr2)
+//    {
+//        int[,] answer = new int[arr1.GetLength(0),arr2.GetLength(1)];
+//        for(int i = 0; i< arr1.GetLength(0); i++)
+//        {
+//            for(int j =0; j< arr2.GetLength(1); j++)
+//            {
+//                answer[i,j] = arr1[i,j] + arr2[i,j];
+//            }
+//        }
 
-        return answer;
-    }
-}
+//        return answer;
+//    }
+//}
+
+// 자릿수 더하기
+//using System;
+
+//public class Solution
+//{
+//    public int solution(int n)
+//    {
+//        int answer = 0;
+//        string num = n.ToString();
+//        for(int i = 0; i < num.Length; i++)
+//        {
+//            answer += int.Parse(num[i].ToString());
+//        }
+//        return answer;
+//    }
+//}
 
 
 
