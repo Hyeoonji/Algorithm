004dd85c91bd30c01a634dabcc4b8d03cd8e9ee0
Shin HyeonJi <jane49621@gmail.com>
2023-04-19 12:35:34 +0900

    0419 1문제 피보나치

--- Programemers.cs
+++ Programemers.cs
@@ -152,21 +152,29 @@
 -> 이렇게 되면 둘다 옆으로 증가
  */
 
-// 콜라츠 추측
-public class Solution
-{
-    public int solution(int num)
-    {
-        int answer = 0;
+// 피보나치 수
+//public class Solution
+//{
+//    public int solution(int n)
+//    {
+//        int answer = 0;
+//        int[] pibo = new int[n + 1];
+//        pibo[0] = 0;
+//        pibo[1] = 1;
+//        for (int i = 2; i < n + 1; i++)
+//        {
+//            pibo[i] = (pibo[i - 2] + pibo[i - 1]) % 1234567;
+//            // 왜 안에서 나눠야 하는가? -> 밖에서 나눌 경우, 숫자가 크면 int, long으로도 커버가 되지 않기 떄문
+//        }
+//        // [2] = 0 + 1 = 1
+//        // [3] = 1 + 1 = 2
+//        // [4] = 1 + 2 = 3
+//        // [5] = 2 + 3 = 5
+//        // [6] = 3 + 5 = 8
 
+//        answer = pibo[n];
+//        return answer;
+//    }
+//}
 
-
-
-
-
-
-
-        return answer;
-    }
-}
-
+//
