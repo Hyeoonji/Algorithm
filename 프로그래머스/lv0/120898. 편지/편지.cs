83ae344385dc8e30301ebc3a5ba81dc1445c2470
Shin Hyeon Ji <122066084+Hyeoonji@users.noreply.github.com>
2023-06-20 09:21:25 +0900

    [level 0] Title: 편지, Time: 0.22 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution
+{
+    public int solution(string message)
+    {
+        int answer = 0;
+
+        answer = message.Length * 2;
+
+        return answer;
+    }
+}
