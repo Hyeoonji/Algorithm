bff6773f480367083e74b086199946d24e373d68
Shin Hyeon Ji <jane49621@gmail.com>
2023-07-26 13:26:53 +0900

    0726) 프로세스

--- Programemers02.cs
+++ Programemers02.cs
@@ -705,7 +705,7 @@ public class Solution
     }
 }*/
 
-// 기능개발
+/*// 기능개발
 public class Solution
 {
     public int[] solution(int[] progresses, int[] speeds)
@@ -748,4 +748,70 @@ public class Solution
         
         return result.ToArray();
     }
-}
+}*/
+
+/*// 프로세스
+// 1. 중요도와 동일한 길이의 배열을 하나 준비
+// 2. 가장 큰 우선순위를 구함
+// 3. 배열의 같은 위치에 숫자 기입
+// 4. 그 다음으로 큰 우선순위 구하기
+// 5. 반복
+// 6. 원하는 위치의 숫자를 출력
+public class Solution
+{
+    public int findTurn(int[] priorities, int num, int start)  //우선순위의 위치 구하기
+    {
+        int count = 0;                                      //발견 위치
+        for (int i = start; i < priorities.Length; i++)
+        {
+            if (priorities[i] == num)
+            {
+                count = i;
+                priorities[i] = 0;                          //검출되지 않도록 0으로 변경
+                break;
+            }
+        }
+
+        if (count == 0)                                      //count가 0 -> 우선순위와 동일한게 없음
+        {
+            for (int i = 0; i < start; i++)                 //앞에부터 구하기
+            {
+                if (priorities[i] == num)
+                {
+                    count = i;
+                    priorities[i] = 0;
+                    break;
+                }
+            }
+        }
+
+        return count;                                       //위치를 출력
+    }
+
+    public int solution(int[] priorities, int location)
+    {
+        int[] sequence = new int[priorities.Length];        
+
+        int max = priorities.Max();
+        int start = 0;
+        int turn = 1;
+
+        for (int i = 0; i < priorities.Length; i++)
+        {
+            int count = findTurn(priorities, max, start);   //같은 값이 또 있는지 확인, 없으면 그 다음으로 큰 수
+
+            if (priorities.Contains(max) == false)
+            {
+                max = priorities.Max();
+            }
+            sequence[count] = turn;
+            start = count;
+            turn++;
+        }
+        
+        int answer = sequence[location];
+        return answer;
+    }
+}*/
+
+//
