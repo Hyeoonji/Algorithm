937ca0ddca457663ff2c91ba28f3441695d44cbb
Shin Hyeon Ji <122066084+Hyeoonji@users.noreply.github.com>
2023-12-28 14:20:37 +0900

    [level unrated] Title: 문자열 돌리기, Time: 23.10 ms, Memory: 16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        foreach(char c in s)
+        {
+            Console.WriteLine(c);
+        }
+    }
+}
