dbe78768a63f9b9234caa9054ed1d7af2dad3d51
Shin Hyeon Ji <122066084+Hyeoonji@users.noreply.github.com>
2024-01-02 19:44:14 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        
+        foreach(int num in numbers)
+        {
+            answer += num;
+            if(answer > n)
+            {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
