3c16ce8b0d112f508a6e36b0ad5c9bdb9116e5ca
Shin Hyeon Ji <122066084+Hyeoonji@users.noreply.github.com>
2024-01-04 15:28:41 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int[,] queries) {
+        int[] answer = new int[] {};
+        for(int i = 0; i < queries.GetLength(0); i++)
+        {
+            int a = queries[i,0];
+            int b = queries[i,1];
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+        
+        answer = arr;
+        
+        return answer;
+    }
+}
