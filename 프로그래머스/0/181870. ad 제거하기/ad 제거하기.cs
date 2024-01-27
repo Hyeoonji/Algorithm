3ffce13bc822613c3a1bedee0a48718c5431cf13
Shin Hyeon Ji <122066084+Hyeoonji@users.noreply.github.com>
2024-01-28 00:09:08 +0900

    [level 0] Title: ad 제거하기, Time: 1.67 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cs
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Linq;
 
 public class Solution {
     public string[] solution(string[] strArr) {
-        string[] answer = strArr.Where(i => !i.Contains("ad")).ToArray();
+        string[] answer = new string[] {};
         return answer;
     }
 }
